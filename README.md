a31cacbf407f4ce05ef360443692351660be5bb9
Mark Hobson <markhobson@gmail.com>
2018-01-13 22:54:19 +0000

    Allow stack to be launched from S3

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cloud-install-demo
+
 Easily install serverless applications to your private cloud.
+
+[![Launch Stack](https://s3.amazonaws.com/cloudformation-examples/cloudformation-launch-stack.png)](https://console.aws.amazon.com/cloudformation/home#/stacks/new?stackName=cloud-install-demo&templateURL=https://s3.eu-west-2.amazonaws.com/cloud-install-demo/stack.yml)
--- stack.sh
+++ stack.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+BUCKET_NAME=cloud-install-demo
 STACK_NAME=cloud-install-demo
+TEMPLATE=stack.yml
 COMMAND=$1
 
 case $COMMAND in
+	"release-up")
+		aws s3 mb s3://$BUCKET_NAME
+		;;
+	"release")
+		aws s3 cp $TEMPLATE s3://$BUCKET_NAME/$TEMPLATE \
+			--grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+		;;
+	"release-down")
+		aws s3 rb s3://$BUCKET_NAME --force
+		;;
 	"up")
 		aws cloudformation create-stack \
 			--stack-name $STACK_NAME \
-			--template-body file://stack.yml \
+			--template-body file://$TEMPLATE \
 			--capabilities CAPABILITY_IAM
 		;;
 	"down")
@@ -15,6 +27,6 @@ case $COMMAND in
 			--stack-name $STACK_NAME
 		;;
 	*)
-		echo "Usage: stack [up | down]"
+		echo "Usage: stack [release-up | release | release-down | up | down]"
 		;;
 esac
